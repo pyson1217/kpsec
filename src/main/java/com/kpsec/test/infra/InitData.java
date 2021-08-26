08ccea134fb64afa2bcf99c8252e27e2cdb0406c
pyson1217 <stop8989@naver.com>
2021-08-26 10:34:08 +0900

    file reader modify

--- src/main/java/com/kpsec/test/infra/InitData.java
+++ src/main/java/com/kpsec/test/infra/InitData.java
@@ -12,7 +12,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
@@ -33,37 +36,46 @@ public class InitData {
     @PostConstruct
     private void initAccount() throws IOException {
         if (accountRepository.count() == 0) {
-            Resource resource = new ClassPathResource("계좌정보.csv");
-            List<Account> accountList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8)
-                    .stream().skip(1).map(line -> {
-                        String[] split = line.split(",");
-                        return Account.builder().accountNo(split[0]).accountName(split[1]).branchCode(split[2])
-                                .build();
-                    }).collect(Collectors.toList());
-            accountRepository.saveAll(accountList);
+            try (InputStream resource = new ClassPathResource("계좌정보.csv").getInputStream()) {
+                List<Account> accountList =
+                        new BufferedReader(new InputStreamReader(resource,
+                                StandardCharsets.UTF_8)).lines().collect(Collectors.toList())
+                        .stream().skip(1).map(line -> {
+                            String[] split = line.split(",");
+                            return Account.builder().accountNo(split[0]).accountName(split[1]).branchCode(split[2])
+                                    .build();
+                        }).collect(Collectors.toList());
+                accountRepository.saveAll(accountList);
+            }
         }
         if(accountHistoryRepository.count() == 0){
-            Resource resource = new ClassPathResource("거래내역.csv");
-            List<AccountHistory> accountList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8)
-                    .stream().skip(1).map(line -> {
-                        String[] split = line.split(",");
-                        return AccountHistory.builder()
-                                .dealDate(split[0]).accountNo(split[1]).dealNo(split[2])
-                                .amount(Integer.parseInt(split[3])).fees(Integer.parseInt(split[4])).dealCancel(split[5])
-                                .build();
-                    }).collect(Collectors.toList());
-            accountHistoryRepository.saveAll(accountList);
+            try (InputStream resource = new ClassPathResource("거래내역.csv").getInputStream()) {
+                List<AccountHistory> accountList =
+                        new BufferedReader(new InputStreamReader(resource,
+                                StandardCharsets.UTF_8)).lines().collect(Collectors.toList())
+                                .stream().skip(1).map(line -> {
+                            String[] split = line.split(",");
+                            return AccountHistory.builder()
+                                    .dealDate(split[0]).accountNo(split[1]).dealNo(split[2])
+                                    .amount(Integer.parseInt(split[3])).fees(Integer.parseInt(split[4])).dealCancel(split[5])
+                                    .build();
+                        }).collect(Collectors.toList());
+                accountHistoryRepository.saveAll(accountList);
+            }
         }
         if(accountBankBranchRepository.count() == 0){
-            Resource resource = new ClassPathResource("관리점정보.csv");
-            List<BankBranch> accountList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8)
-                    .stream().skip(1).map(line -> {
-                        String[] split = line.split(",");
-                        return BankBranch.builder()
-                                .branchCode(split[0]).branchName(split[1])
-                                .build();
-                    }).collect(Collectors.toList());
-            accountBankBranchRepository.saveAll(accountList);
+            try (InputStream resource = new ClassPathResource("관리점정보.csv").getInputStream()) {
+                List<BankBranch> accountList =
+                        new BufferedReader(new InputStreamReader(resource,
+                                StandardCharsets.UTF_8)).lines().collect(Collectors.toList())
+                                .stream().skip(1).map(line -> {
+                            String[] split = line.split(",");
+                            return BankBranch.builder()
+                                    .branchCode(split[0]).branchName(split[1])
+                                    .build();
+                        }).collect(Collectors.toList());
+                accountBankBranchRepository.saveAll(accountList);
+            }
         }
     }
 }
